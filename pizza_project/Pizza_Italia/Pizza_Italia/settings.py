0b9d903c5c41203155271ed4c884cebe42124a43
Karine Vardanyan <kvardany@internal.synopsys.com>
2018-12-02 10:03:55 +0400

    'settings'

--- pizza_project/Pizza_Italia/Pizza_Italia/settings.py
+++ pizza_project/Pizza_Italia/Pizza_Italia/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ucbv40jnfmrlv)vzoab8#g#rkvvk_xwblvqi+sw$rl%a2y3cdt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
